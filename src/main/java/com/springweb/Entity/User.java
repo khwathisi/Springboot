d55683ea799229118c923473521e2436cbe0297c
Recruit <Godfrey.Khwathisi@umuzi.org>
2020-05-13 22:03:05 +0200

    update

---
+++ src/main/java/com/springweb/Entity/User.java
@@ -0,0 +1,7 @@
+package com.springweb.Entity;
+
+public class User {
+    private long Id;
+    private String name;
+    private String surname;
+}
---
+++ src/main/java/com/springweb/Main.java
@@ -0,0 +1,13 @@
+package com.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+
+}
