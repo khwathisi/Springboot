b6b86691f8b2eb50b346a98b3a420ce9af801fa0
Recruit <Godfrey.Khwathisi@umuzi.org>
2020-05-13 22:08:06 +0200

    update

--- src/main/java/com/springweb/Entity/User.java
+++ src/main/java/com/springweb/Entity/User.java
@@ -4,4 +4,16 @@ public class User {
     private long Id;
     private String name;
     private String surname;
+
+    //Getters
+    public long getId() { return Id; }
+    public String getName() { return name; }
+    public String getSurname() { return surname; }
+
+    //Setters
+    public void setId(long id) { Id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setSurname(String surname) { this.surname = surname; }
 }
+
+
